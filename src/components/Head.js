7c2cfe4393e96ebb7b4bd36610a27962cf40fc61
amaanraaj <amandwivedi00@gmail.com>
2023-04-12 02:08:24 +0530

    updated link to logo

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,7 +5,9 @@ const Head = () => {
     <>
     <div className='bg-pink h-40 flex flex-col justify-between items-center pr-8 sm:flex-row sm:h-12'>
         <div>
+          <a href='/'>
             <img src='https://www.freepnglogos.com/uploads/chocolate-png/download-chocolate-bar-transparent-image-png-image-4.png' alt="logo" className='h-14 w-20'/>
+            </a>
         </div>
         <div>
         <h1 className='text-center font-semibold text-neutral-800 font-serif text-xl '>Create your custom pack of chocolates </h1>
